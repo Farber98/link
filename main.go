217f163137c69ac18e92e114fc43445d0de4f622
Farber98 <juantuc98@gmail.com>
2022-07-29 09:33:30 -0300

    extracting only links. Text not supported yet.

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"golang.org/x/net/html"
+)
+
+const EX1 = "ex1.html"
+const EX2 = "ex2.html"
+const EX3 = "ex3.html"
+const EX4 = "ex4.html"
+
+type link struct {
+	href string
+	text string
+}
+
+func readHtmlFromFile(fileName string) (string, error) {
+
+	bs, err := ioutil.ReadFile(fileName)
+
+	if err != nil {
+		return "", err
+	}
+
+	return string(bs), nil
+}
+
+func main() {
+	htmlCont, err := readHtmlFromFile(EX4)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc, err := html.Parse(strings.NewReader(htmlCont))
+	if err != nil {
+		log.Fatal(err)
+
+	}
+	linksArr := []link{}
+	var f func(*html.Node)
+	f = func(n *html.Node) {
+		if n.Type == html.ElementNode && n.Data == "a" {
+			for _, a := range n.Attr {
+				if a.Key == "href" {
+					l := link{href: a.Val, text: "not supported yet..."}
+					linksArr = append(linksArr, l)
+					break
+				}
+			}
+		}
+		for c := n.FirstChild; c != nil; c = c.NextSibling {
+			f(c)
+		}
+	}
+	f(doc)
+	fmt.Println(linksArr)
+}
